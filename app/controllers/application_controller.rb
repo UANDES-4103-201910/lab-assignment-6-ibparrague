f5d072bdc2ddf33f9bf780fdf8a869fee60f7ae0
IgnacioParrague <ibparrague@miuandes.cl>
2019-04-19 13:37:31 -0400

    done(?)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,22 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-	#complete this method
+    return unless session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id])
   end
+
 
   def is_user_logged_in?
 	#complete this method
-  	logged_in = false
-	if logged_in then true else redirect_to root_path end 
+    !current_user.nil?
   end
+
+
+ #default
+#  def is_user_logged_in?
+  #complete this method
+#    logged_in = false
+#  if logged_in then true else redirect_to root_path end
+#
+  #end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,29 @@ class SessionsController < ApplicationController
 	def new
 	end
 
+	def log_in(user)
+    session[:user_id] = user.id
+  end
+
+	def log_out
+		session.delete(:user_id)
+		@current_user = nil
+	end
+
 	def create
 		#complete this method
+		user = User.find_by(email: params[:session][:email].downcase, password: params[:session][:password])
+    if user.present?
+      log_in user
+      redirect_to user , notice: "Log in Successfully, no idea why this appears twice."
+    else
+      redirect_to root_url, notice: 'Invalid email/password combination'
+    end
 	end
 
 	def destroy
 		#complete this method
+		log_out
+		redirect_to root_url
 	end
 end
